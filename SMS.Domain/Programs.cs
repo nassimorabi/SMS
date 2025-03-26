1e0bce1b99a83db2b714484415ee39fe90927e42
nassimorabi <nassimehaborabi@gmail.com>
2025-03-26 14:32:07 +0800

    added Programs

---
+++ SMS.Domain/Programs.cs
@@ -0,0 +1,10 @@
+﻿namespace SMS.Domain
+{
+    public class Programs
+    {
+        public int ProgramId { get; set; }
+        public string ProgramName { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
